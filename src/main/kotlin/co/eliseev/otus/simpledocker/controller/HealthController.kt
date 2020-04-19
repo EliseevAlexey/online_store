d16b97f3899d663d1afe9ee3a2c97e7a72f19998
Alexey Eliseev <alexey_eliseev@epam.com>
2020-04-19 23:59:11 +0300

    part 2

--- src/main/kotlin/co/eliseev/otus/simpledocker/controller/HealthController.kt
+++ src/main/kotlin/co/eliseev/otus/simpledocker/controller/HealthController.kt
@@ -3,12 +3,21 @@ package co.eliseev.otus.simpledocker.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping
 class HealthController {
 
     @GetMapping
-    fun healthCheck() = "version 1"
+    fun checkHost() = "Hello from $hostName\n"
+
+    @GetMapping("/health")
+    fun healthCheck() = "version $VERSION from $hostName\n"
+
+    companion object {
+        private const val VERSION = 2
+        private val hostName = InetAddress.getLocalHost().hostName
+    }
 
 }
